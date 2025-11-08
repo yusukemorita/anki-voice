c3699b6422961c558339b8251777280c5537be0f
yusukemorita <yusuke.ppq@gmail.com>
2025-11-08 20:22:35 +0100

    extract audio generation into a separate package

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"anki-voice/ankiconnect"
-	"bytes"
+	"anki-voice/audio"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 )
@@ -59,71 +56,13 @@ func main() {
 
 		log.Printf("generating audio for: %s...\n", phrase.Value)
 
-		outputPathWav := fmt.Sprintf("./output/%s.wav", phrase.Value)
+		outputPath := fmt.Sprintf("./output/%s.mp3", phrase.Value)
 
-		err = generateWav(phrase.Value, outputPathWav)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer os.Remove(outputPathWav)
-
-		log.Println("converting mp3...")
-		// TODO: check that ffmpeg is available
-		err = convertWavToMp3(outputPathWav, fmt.Sprintf("./output/%s.mp3", phrase.Value))
+		err = audio.GenerateMP3(phrase.Value, outputPath)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-}
-
-func generateWav(text, outputPath string) error {
-	// TODO: try to adjust speed with length_scale? currently sending this just causes the voice
-	// to read through the whole payload
-	// payload := map[string]any{
-	// 	"text":  text,
-	// 	"length_scale": 1.2,
-	// }
-
-	// body, err := json.Marshal(payload)
-	// if err != nil {
-	// 	return fmt.Errorf("marshal request failed: %w", err)
-	// }
-
-	// response, err := http.Post("http://localhost:9999", "application/json", bytes.NewReader(body))
-	// if err != nil {
-	// 	return err
-	// }
-	// defer response.Body.Close()
-
-	response, err := http.Post("http://localhost:9999", "application/json", strings.NewReader(text))
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	out, err := os.Create(outputPath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	if _, err := io.Copy(out, response.Body); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func convertWavToMp3(input, output string) error {
-	var stderr bytes.Buffer
-	cmd := exec.Command("ffmpeg", "-y", "-i", input, "-codec:a", "libmp3lame", "-b:a", "192k", output)
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("ffmpeg failed: %v\nDetails:\n%s", err, stderr.String())
-	}
-	return nil
 }
 
 func keys(m map[string]string) []string {
